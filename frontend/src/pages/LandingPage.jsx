9d9bece3425a9ae9e5f8ce32fc516d13071ca9e1
yjtan0819 <tan.yijia0819@gmail.com>
2023-12-14 12:55:05 -0500

    add Landing

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../css/LandingPage.module.css';
+
+const LandingPage = () => {
+  return (
+    <div className={styles.landingContainer}>
+      <div className={styles.headerContent}>
+        <img src="crisp-high-resolution-logo.png" alt="Company Logo" className={styles.logo}/>
+        <div className={styles.buttonsLanding}>
+          <Link to="/login">
+            <button className={styles.loginButton}>Login</button>
+          </Link>
+          <Link to="/register">
+            <button className={styles.registerButton}>Register</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
